d0bd9fc83264625bc1005ed8b913ed5f4d93a509
Guillaume Jariot <guillaume.jariot@ntymail.com>
2017-06-18 00:57:37 +0200

    Add colour game

---
+++ src/Games/ColourGame.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import EndGameMessage from './EndGameMessage';
+import colourAssociations from '../res/colourAssociations.json';
+import Random from 'random-js';
+
+class ColourGame extends React.Component {
+    constructor() {
+        super();
+
+        this.colourAssociations = colourAssociations;
+        this.coloursToFind = new Random().shuffle(Object.keys(colourAssociations));
+        this.currentColourAssociationIndex = 0;
+        
+        const colourToFind = this.coloursToFind[this.currentColourAssociationIndex];
+
+        this.state = {
+            coloursMix: this.colourAssociations[colourToFind],
+            answer: colourToFind,
+            possibleAnswers: this.getRandomColours(colourToFind),
+            resultClass: 'hidden',
+            resultMessage: '',
+            questionsEnded: false
+        };
+
+        this.checkAnswer = this.checkAnswer.bind(this);
+        this.moveToNextQuestion = this.moveToNextQuestion.bind(this);
+        this.handleRestart = this.handleRestart.bind(this);
+        this.giveUp = this.giveUp.bind(this);
+    }
+    
+    getRandomColours(answerColour) {
+        const random = Random();
+        let availableColours = this.coloursToFind.slice(0);
+        availableColours.splice(availableColours.indexOf(answerColour), 1);
+        
+        availableColours = random.shuffle(availableColours).slice(0, 2);
+        availableColours.push(answerColour);
+
+        return random.shuffle(availableColours);
+    }
+    
+    giveUp(event) {
+        event.preventDefault();
+        
+        this.setState({
+            resultClass: 'answer-result error',
+            resultMessage: 'La bonne réponse était "' + this.state.answer + '"'
+        });
+        
+        setTimeout(this.moveToNextQuestion, 1500);
+    }
+
+    checkAnswer(event) {
+        event.preventDefault();
+        
+        if (event.target.value === this.state.answer) {
+            this.setState({
+                resultClass: 'answer-result success',
+                resultMessage: 'Bravo !'
+            });
+
+            setTimeout(this.moveToNextQuestion, 1250);
+        } else {
+            this.setState({
+                resultClass: 'answer-result error',
+                resultMessage: "Oh non, ce n'est pas la bonne réponse!"
+            });
+        }
+    }
+
+    moveToNextQuestion() {
+        this.currentColourAssociationIndex += 1;
+        
+        if (this.coloursToFind.length <= this.currentColourAssociationIndex) {
+            this.setState({questionsEnded: true});
+            return;
+        }
+        
+        this.moveToQuestion(this.currentColourAssociationIndex);
+    }
+    
+    moveToQuestion(index) {
+        const colourToFind = this.coloursToFind[index];
+        
+        this.setState({
+            coloursMix: this.colourAssociations[colourToFind],
+            answer: colourToFind,
+            possibleAnswers: this.getRandomColours(colourToFind),
+            resultClass: 'answer-result hidden',
+            resultMessage: '',
+            questionsEnded: false
+        });
+    }
+    
+    handleRestart() {
+        this.colourAssociations = new Random().shuffle(this.colourAssociations);
+        this.currentColourAssociationIndex = 0;
+        
+        this.moveToQuestion(this.currentColourAssociationIndex);
+    }
+
+    render() {
+        return (
+            <div className="questionGame">
+                {this.state.questionsEnded === true && <EndGameMessage handleRestart={this.handleRestart} />}
+                {this.state.questionsEnded === false &&
+                    <QuestionPrompt
+                        coloursMix={this.state.coloursMix}
+                        answer={this.state.answer}
+                        proposedAnswers={this.state.possibleAnswers}
+                        checkAnswer={this.checkAnswer}
+                        resultClass={this.state.resultClass}
+                        resultMessage={this.state.resultMessage}
+                        giveUp={this.giveUp}
+                    />
+                }
+            </div>
+        );
+    }
+}
+
+function QuestionPrompt(props) {
+    return (
+        <section>
+            <form className="question colour">
+                Qu'obtient-on en mélangeant 
+                {props.coloursMix.map(function(colour) {
+                    const colourStyle = {color: colour, backgroundColor: colour};
+                    return <span className="colourSource" style={colourStyle} />;
+                }).reduce((prev, curr) => [prev, ' + ', curr])}
+                ?
+                <div className="possible-colours">
+                    {props.proposedAnswers.map(function(colour) {
+                        const colourStyle = {color: colour, backgroundColor: colour};
+                        return <input type="submit" className="colourPossibleAnswer" onClick={props.checkAnswer} style={colourStyle} value={colour} />;
+                    }).reduce((prev, curr) => [prev, ' ou ', curr])}
+                </div>
+                <button type="submit" className="give-up" onClick={props.giveUp}>Donner sa langue au chat</button>
+            </form>
+            <p className={props.resultClass}>{props.resultMessage}</p>
+        </section>
+    );
+}
+
+export default ColourGame;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import QuestionGame from './Games/QuestionGame';
 import CalculationGame from './Games/CalculationGame';
+import ColourGame from './Games/ColourGame';
 import './index.css';
 
 class Game extends React.Component {
@@ -10,8 +11,13 @@ class Game extends React.Component {
         
         this.GAME_QUESTIONS = '1';
         this.GAME_CALCULATION = '2';
+        this.GAME_COLOUR = '3';
         
-        this.state = {questionClassName: '', calculationClassName: 'game-hidden'};
+        this.state = {
+            questionClassName: '',
+            calculationClassName: 'game-hidden',
+            colourClassName: 'game-hidden'
+        };
         
         this.toggleGame = this.toggleGame.bind(this);
     }
@@ -21,7 +27,8 @@ class Game extends React.Component {
         
         this.setState({
             questionClassName: (event.target.getAttribute('data-game') === this.GAME_QUESTIONS ? '' : 'game-hidden'),
-            calculationClassName: (event.target.getAttribute('data-game') === this.GAME_CALCULATION ? '' : 'game-hidden')
+            calculationClassName: (event.target.getAttribute('data-game') === this.GAME_CALCULATION ? '' : 'game-hidden'),
+            colourClassName: (event.target.getAttribute('data-game') === this.GAME_COLOUR ? '' : 'game-hidden')
         });
     }
     
@@ -31,12 +38,16 @@ class Game extends React.Component {
                 <ul className="main-menu">
                     <li><a href="questions" data-game={this.GAME_QUESTIONS} onClick={this.toggleGame}>Questions</a></li>
                     <li><a href="calculs" data-game={this.GAME_CALCULATION} onClick={this.toggleGame}>Calculs</a></li>
+                    <li><a href="calculs" data-game={this.GAME_COLOUR} onClick={this.toggleGame}>Couleurs</a></li>
                 </ul>
                 <div className={this.state.questionClassName}>
                     <QuestionGame />
                 </div>
                 <div className={this.state.calculationClassName}>
                     <CalculationGame />
+                </div>
+                <div className={this.state.colourClassName}>
+                    <ColourGame />
                 </div>
             </div>
         );
